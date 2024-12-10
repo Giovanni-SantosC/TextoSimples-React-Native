1f60c88adf8a9c76d26dd2193f2a41ca342fed3a
Giovanni S. Carvalho <164267847+Giovanni-SantosC@users.noreply.github.com>
2024-12-10 05:10:51 -0300

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+
+const YourApp = () =>{
+  return(
+    <View style={{flex:1 ,backgroundColor:'white', justifyContent:'center',alignItems:'center',}}>
+      <Text>Aula de ADS</Text>
+
+    </View>
+  )
+}
+
+export default YourApp
